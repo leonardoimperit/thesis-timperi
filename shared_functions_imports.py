3573059dabd784ec3cef02d9a11874a2b000a40e
leonardoimperit <87013961+leonardoimperit@users.noreply.github.com>
2021-07-06 17:07:51 +0200

    Create shared_functions_imports.py

---
+++ shared_functions_imports.py
@@ -0,0 +1,46 @@
+# General
+import os
+import pandas as pd
+import numpy as np
+import math
+import sys
+import time
+import pickle
+import datetime
+
+#import sklearn
+import sklearn
+from sklearn import *
+
+%matplotlib inline
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+sns.set_style('darkgrid', {'axes.facecolor': '0.9'})
+
+import graphviz
+import xgboost
+
+import warnings
+warnings.filterwarnings('ignore')
+
+# Load a set of pickle files, put them together in a single DataFrame, and order them by time
+# It takes as input the folder DIR_INPUT where the files are stored, and the BEGIN_DATE and END_DATE
+def read_from_files(DIR_INPUT, BEGIN_DATE, END_DATE):
+    
+    files = [os.path.join(DIR_INPUT, f) for f in os.listdir(DIR_INPUT) if f>=BEGIN_DATE+'.pkl' and f<=END_DATE+'.pkl']
+
+    frames = []
+    for f in files:
+        df = pd.read_pickle(f)
+        frames.append(df)
+        del df
+    df_final = pd.concat(frames)
+    
+    df_final=df_final.sort_values('TRANSACTION_ID')
+    df_final.reset_index(drop=True,inplace=True)
+    #  Note: -1 are missing values for real world data 
+    df_final=df_final.replace([-1],0)
+    
+    return df_final
