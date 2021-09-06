1fbcc797dd9bb8591037ec0f540356dbd447c13d
leonardoimperit <87013961+leonardoimperit@users.noreply.github.com>
2021-09-06 22:41:26 +0200

    Add files via upload

---
+++ shared_functions_imports.py
@@ -0,0 +1,149 @@
+# General
+import os
+import pandas as pd
+import numpy as np
+import math
+import sys
+import time
+import pickle
+import datetime
+
+# Importing Libraries
+
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.manifold import TSNE
+from sklearn.decomposition import PCA, TruncatedSVD
+import matplotlib.patches as mpatches
+import time
+
+# Classifier Libraries
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+import collections
+
+
+# Other Libraries
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
+from sklearn.pipeline import make_pipeline
+from imblearn.pipeline import make_pipeline as imbalanced_make_pipeline
+from imblearn.over_sampling import SMOTE
+from imblearn.under_sampling import NearMiss
+from imblearn.metrics import classification_report_imbalanced
+from sklearn.metrics import precision_score, recall_score, f1_score, roc_auc_score, accuracy_score, classification_report
+from collections import Counter
+from sklearn.model_selection import KFold, StratifiedKFold
+import warnings
+warnings.filterwarnings("ignore")
+
+#GAN Libraries
+import os
+import random
+from tqdm import tqdm_notebook as tqdm
+from keras.models import Model
+from keras.layers import Input, Reshape, GaussianNoise
+from keras.layers.core import Dense, Activation, Dropout, Flatten
+from keras.layers.normalization import BatchNormalization
+from keras.layers.convolutional import UpSampling1D, Conv1D
+from keras.layers.advanced_activations import LeakyReLU
+from keras.optimizers import Adam, SGD
+from keras.callbacks import TensorBoard
+
+#import sklearn
+import sklearn
+from sklearn import *
+
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+sns.set_style('darkgrid', {'axes.facecolor': '0.9'})
+
+import graphviz
+import xgboost
+
+# Importing Libraries
+
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+import tensorflow as tf
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.manifold import TSNE
+from sklearn.decomposition import PCA, TruncatedSVD
+import matplotlib.patches as mpatches
+import time
+
+# Classifier Libraries
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+import collections
+
+
+# Other Libraries
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
+from sklearn.pipeline import make_pipeline
+from imblearn.pipeline import make_pipeline as imbalanced_make_pipeline
+from imblearn.over_sampling import SMOTE
+from imblearn.under_sampling import NearMiss
+from imblearn.metrics import classification_report_imbalanced
+from sklearn.metrics import precision_score, recall_score, f1_score, roc_auc_score, accuracy_score, classification_report
+from collections import Counter
+from sklearn.model_selection import KFold, StratifiedKFold
+import warnings
+warnings.filterwarnings("ignore")
+
+#GAN Libraries
+import os
+import random
+from tqdm import tqdm_notebook as tqdm
+from keras.models import Model
+from keras.layers import Input, Reshape, GaussianNoise
+from keras.layers.core import Dense, Activation, Dropout, Flatten
+from keras.layers.normalization import BatchNormalization
+from keras.layers.convolutional import UpSampling1D, Conv1D
+from keras.layers.advanced_activations import LeakyReLU
+from keras.optimizers import Adam, SGD
+from keras.callbacks import TensorBoard
+
+import warnings
+warnings.filterwarnings('ignore')
+
+# Load a set of pickle files, put them together in a single DataFrame, and order them by time
+# It takes as input the folder DIR_INPUT where the files are stored, and the BEGIN_DATE and END_DATE
+def read_from_files(DIR_INPUT, BEGIN_DATE, END_DATE):
+    
+    files = [os.path.join(DIR_INPUT, f) for f in os.listdir(DIR_INPUT) if f>=BEGIN_DATE+'.pkl' and f<=END_DATE+'.pkl']
+
+    frames = []
+    for f in files:
+        df = pd.read_pickle(f)
+        frames.append(df)
+        del df
+    df_final = pd.concat(frames)
+    
+    df_final=df_final.sort_values('TRANSACTION_ID')
+    df_final.reset_index(drop=True,inplace=True)
+    #  Note: -1 are missing values for real world data 
+    df_final=df_final.replace([-1],0)
+    
+    return df_final
+
+def scaleData(train,test,features):
+    scaler = sklearn.preprocessing.StandardScaler()
+    scaler.fit(train[features])
+    train[features]=scaler.transform(train[features])
+    test[features]=scaler.transform(test[features])
+    
+    return (train,test)
